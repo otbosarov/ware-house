8102d0fa20f012374ab32d1cbd4934ea8c356074
otbosarov <abdullohotbosarov720@gmail.com>
2024-12-30 18:00:14 +0500

    Excelga vaqtni tanlab malumot yuklash

--- app/Exports/InputProductExcelExport.php
+++ app/Exports/InputProductExcelExport.php
@@ -7,32 +7,33 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 
 class InputProductExcelExport implements FromCollection
 {
+    public function __construct(private $data)
+    {
+        $this->data = $data;
+    }
+
     /**
      * @return \Illuminate\Support\Collection
      */
     public function collection()
     {
-
-        request()->start_date;
-        request()->end_date;
-
-        $data =   InputProduct::join('product_variants', 'input_products.product_variant_id', '=', 'product_variants.id')
-            ->join('products', 'product_variants.product_id', '=', 'products.id')
-            ->join('categories', 'products.product_category_id', '=', 'categories.id')
-            ->join('brends', 'products.product_brend_id', '=', 'brends.id')
-            ->join('product_variant_details', 'product_variants.id', '=', 'product_variant_details.product_variant_id')
-            ->select(
-                'input_products.id',
-                'product_variant_title',
-                'category_title',
-                'brend_title',
-                'currency_type',
-                'input_price',
-                'selling_price',
-                'amount'
-            )
-            ->get();
-        return   $data->prepend([
+        // $data =   InputProduct::join('product_variants', 'input_products.product_variant_id', '=', 'product_variants.id')
+        //     ->join('products', 'product_variants.product_id', '=', 'products.id')
+        //     ->join('categories', 'products.product_category_id', '=', 'categories.id')
+        //     ->join('brends', 'products.product_brend_id', '=', 'brends.id')
+        //     ->join('product_variant_details', 'product_variants.id', '=', 'product_variant_details.product_variant_id')
+        //     ->select(
+        //         'input_products.id',
+        //         'product_variant_title',
+        //         'category_title',
+        //         'brend_title',
+        //         'currency_type',
+        //         'input_price',
+        //         'selling_price',
+        //         'amount'
+        //     )
+        //     ->get();
+        return  $this->data->prepend([
             'id' => 'ID',
             'product_variant_title' => "Tovar nomi",
             'category_title' => "Kategoriya",
